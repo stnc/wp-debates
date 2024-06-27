c885190ec0798881979d47617949369a139a912a
stnc-winDesktop <selmantunc@gmail.com>
2024-06-27 14:10:42 -0600

    all of backup

--- sql.ps1
+++ sql.ps1
@@ -1,7 +1,10 @@
 
+@echo off
+
 set path=D:\laragon\bin\mysql\mysql-8.0.30-winx64\bin
 
-.\mysqldump.exe  -u root  -p debates > D:\debates-sqlfile.sql  
+cd D:\laragon\bin\mysql\mysql-8.0.30-winx64\bin
+.\mysqldump.exe  -u root  -p debates > D:\laragon\www\debates\wp-content\plugins\tvs-debate\other\debates-sqlfile.sql  
 
 # UPDATE wp_options SET option_value = replace(option_value, 'http://www.oldurl', 'http://www.newurl') WHERE option_name = 'home' OR option_name = 'siteurl';
 

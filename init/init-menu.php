d731a07b15e8e5a76613eaf8a14f537ddf01b819
TUNÇ Selman <selmantunc@yandex.com>
2024-10-02 02:43:18 -0600

    ddd

--- init/init-menu.php
+++ init/init-menu.php
@@ -13,3 +13,26 @@ add_action('admin_menu', 'tvsDebate_configuration_menu');
 
 
 require_once (tvsDebate_init_Path .'c_debate-opinions-type.php');
+
+
+add_filter('manage_debate_posts_columns', 'CHfw2_add_img_column');
+add_filter('manage_debate_posts_custom_column', 'CHfw2_manage_img_column', 10, 2);
+
+/*
+add custom_colum
+@use http://bit.ly/2zKE0k4
+*/
+function CHfw2_add_img_column($columns)
+{
+    $columns['img'] = 'Featured Image';
+    return $columns;
+}
+
+function CHfw2_manage_img_column($column_name, $post_id)
+{
+    if ($column_name == 'img') {
+        echo get_the_post_thumbnail($post_id, 'thumbnail');
+    }
+
+    return $column_name;
+}
